511841613d8765d355c66966b7b26604075eeddb
Chris-wilson Obidiebube <christianwilsonobi@gmail.com>
2023-10-20 10:31:52 +0100

    ceaser

---
+++ src/pages/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, est
+        autem? Perferendis distinctio quae recusandae facilis adipisci numquam,
+        maiores saepe similique, reprehenderit quisquam doloribus? Ab, obcaecati
+        nesciunt? Dignissimos quis doloribus aliquid. Corrupti deserunt eius
+        maiores deleniti aut quibusdam, labore fugit enim perferendis distinctio
+        id totam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus
+        nemo ducimus quod repellendus ad, tempora facere quo hic sed dolore.
+      </p>
+    </section>
+  );
+};
+
+export default About;
